2493bbedb2d82bd18b24f527ef8328c8ba90a875
duke <gj009351@163.com>
2020-06-07 17:42:31 +0800

    app icon

--- app/src/main/java/com/duke/bookmark/MainActivity.java
+++ app/src/main/java/com/duke/bookmark/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-       FloatingActionButton fab = findViewById(R.id.fab);
+        FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
