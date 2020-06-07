a4f433d2f2a13341fc0ca8e05710aa2130ffda6f
duke <gj009351@163.com>
2020-06-07 16:26:27 +0800

    init project

--- app/src/main/java/com/duke/bookmark/views/OnCollectionItemClickListener.java
+++
@@ -1,8 +0,0 @@
-package com.duke.bookmark.views;
-
-/**
- * Created by Matteo on 27/07/2015.
- */
-public interface OnCollectionItemClickListener {
-    void onClick(CollectionItem collectionItem, int position);
-}
